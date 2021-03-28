fa268d2310e7aa9e1e3605eca5b79c1c6ca01020
AdityaMankotia <adityamankotia370@gmail.com>
2021-03-28 11:39:16 +0530

    2nd part done 3rd left

--- scripts/header.js
+++ scripts/header.js
@@ -40,23 +40,3 @@ function signUp() {
     signin.style.display = "none";
     signup.style.display = "block";
   }
-
-// get create post modal 
-var createPost = document.getElementById('createPostModal');
-
-// Get the signup button that opens the modal
-var createPostBtn = document.getElementById("createBtn");
-
-// Get the <span> element that closes the create modal
-var createmodalSpan = document.getElementById("createModalSpan");
-
-// When the user clicks on <span> (x), close the modal
-createmodalSpan.onclick = function() {
-  createPost.style.display = "none";
-}
-
-// When the user clicks on the create Post button, open the modal
-createPostBtn.onclick = function() {
-  createPost.style.display = "block";
-}
-  
